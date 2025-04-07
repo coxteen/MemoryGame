83f0a6a229b16076c8ea3cdfb89dd568f7696a09
Costin-Daniel Ghiujan <costin.ghiujan@student.unitbv.ro>
2025-04-07 15:08:39 +0300

    Card Class Moved

---
+++ MemoryGame/MemoryGame/Model/Card.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MemoryGame.Model
+{
+    public class Card : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _imagePath;
+        private bool _isFlipped;
+        private bool _isMatched;
+        private BitmapImage _imageSource;
+
+        #region Properties
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ImagePath
+        {
+            get => _imagePath;
+            set
+            {
+                _imagePath = value;
+                LoadImageSource();
+                OnPropertyChanged();
+            }
+        }
+
+        public BitmapImage ImageSource
+        {
+            get => _imageSource;
+            private set
+            {
+                _imageSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsFlipped
+        {
+            get => _isFlipped;
+            set
+            {
+                _isFlipped = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsMatched
+        {
+            get => _isMatched;
+            set
+            {
+                _isMatched = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Load Image Source
+        private void LoadImageSource()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(ImagePath))
+                {
+                    var bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(ImagePath, UriKind.Absolute);
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.EndInit();
+                    bitmap.Freeze();
+                    ImageSource = bitmap;
+                }
+            }
+            catch (Exception)
+            {
+                ImageSource = null;
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
